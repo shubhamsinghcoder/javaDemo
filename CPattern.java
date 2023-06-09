f5241179c4ecc8932933493b1b1900f602fd7043
shubhamsinghcoder <contact.codershubham@gmail.com>
2023-06-09 10:46:23 +0530

    CPattern

---
+++ CPattern.java
@@ -0,0 +1,21 @@
+package Pattern;
+
+public class CPattern {
+	public static void main(String[] args) {
+		
+		int n = 5;
+		
+		for(int i = 0; i<n; i++) {
+			for(int j = 0; j<n; j++) {
+				if(j ==0 && i>0 && i<n-1 || i==0 && j>0 || i==n-1 && j>0) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
